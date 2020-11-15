3481a377c4007246c59ed6f33014b522803b6863
charlie-li-02 <charlie.zh.li.1025@gmail.com>
2020-11-14 23:22:11 -0800

    setup project structure

--- src/App.java
+++ src/App.java
@@ -1,5 +1,40 @@
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import ui.homePageController;
+
+public class App extends Application {
+
+    Stage window;
+    Scene home;
+
+
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        window = primaryStage;
+        window.setTitle("GUAVA");
+        homeScene();
+    }
+
+    private void homeScene() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("ui/home.fxml"));
+            Parent root = loader.load();
+            homePageController controller = loader.getController();
+            home = new Scene(root);
+            home.getStylesheets().add(getClass().getResource("cssFiles/home.css").toExternalForm());
+            window.setScene(home);
+            window.show();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 }
---
+++ src/ui/homePageController.java
@@ -0,0 +1,42 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class homePageController {
+    @FXML
+    private Label homeLabel;
+
+    @FXML
+    private Label recipesLabel;
+
+    @FXML
+    private Label familyLabel;
+
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private Button recipesButton;
+
+    @FXML
+    private Button familyButton;
+
+    @FXML
+    public void homeButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void recipeButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void familyButtonPressed(ActionEvent event) {
+
+    }
+
+}
